2bb24387e292a2fdc4e56f8c570ee57b4609f96b
Andrew Semyonov <101043571+Andrew-Sem@users.noreply.github.com>
2023-11-06 21:53:38 +0300

    prettier formatting

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,16 +1,25 @@
-import type { Metadata } from 'next';
-import './globals.css';
-import { GeistSans, GeistMono } from 'geist/font';
+import type { Metadata } from "next"
+
+import "./globals.css"
+
+import { GeistMono, GeistSans } from "geist/font"
 
 export const metadata: Metadata = {
-	title: 'Dating',
-	description: 'Find your like-minded people',
-};
+    title: "Dating",
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang='en'>
-			<body className={`${GeistSans.variable} ${GeistMono.variable}`}>{children}</body>
-		</html>
-	);
+    description: "Find your like-minded people",
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body className={`${GeistSans.variable} ${GeistMono.variable}`}>
+                {children}
+            </body>
+        </html>
+    )
 }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-	return (
-		<main className='flex min-h-screen items-center justify-center'>
-			<h1 className='text-6xl font-black'>Some text</h1>
-		</main>
-	);
+    return (
+        <main className="flex min-h-screen items-center justify-center">
+            <h1 className="text-6xl font-black">Some text</h1>
+        </main>
+    )
 }
