ed45e8677ceb0a776db59fb5bf5d75d68618ddb8
Andrew Semyonov <101043571+Andrew-Sem@users.noreply.github.com>
2023-11-06 21:50:38 +0300

    font

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,7 @@
-import Image from 'next/image';
-
 export default function Home() {
-	return <main></main>;
+	return (
+		<main className='flex min-h-screen items-center justify-center'>
+			<h1 className='text-6xl font-black'>Some text</h1>
+		</main>
+	);
 }
