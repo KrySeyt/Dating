8b3f189fcaa0f62e0be9f4e74e35e83e19b7f0c7
Andrew Semyonov <101043571+Andrew-Sem@users.noreply.github.com>
2023-11-11 20:39:40 +0300

    init

--- client/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-    return (
-        <main className="flex min-h-screen items-center justify-center">
-            <h1 className="text-6xl font-black">Some text</h1>
-        </main>
-    )
-}
