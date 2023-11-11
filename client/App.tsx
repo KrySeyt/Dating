1d66f94a64686c8d07c2abb808cd24b01767c9b0
Andrew Semyonov <101043571+Andrew-Sem@users.noreply.github.com>
2023-11-11 20:58:17 +0300

    add tailwind

--- client/App.tsx
+++ client/App.tsx
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { NativeWindStyleSheet } from 'nativewind';
+
+NativeWindStyleSheet.setOutput({
+	default: 'native',
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View className='flex-1 items-center justify-center'>
+			<Text className='text-red-500 font-bold'>
+				Open up App.tsx to start working on your app!
+			</Text>
+			<StatusBar style='auto' />
+		</View>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
